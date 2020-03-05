af4ee4ca743b67f641f6685b835366ef37880597
JangGeonil <>
2020-03-05 18:08:53 +0900

    자료형과 변수

--- app/src/test/java/Kotlin2020/001. 변수와 자료형.kt
+++ app/src/test/java/Kotlin2020/001. 변수와 자료형.kt
@@ -2,7 +2,7 @@ package Kotlin2020
 
 fun main(args : Array<String>){
 
-    var a: Int = 10 // variable : 변할 수 있는 변수 방법 -> let
-    val b: Int = 20 // value : 변할 수 없는 변수 선언 방법 -> const
+    var a: Int = 10 // variable : 변할 수 있는 변수 방법 -> let (js)
+    val b: Int = 20 // value : 변할 수 없는 변수 선언 방법 -> const (js)
 
 }
